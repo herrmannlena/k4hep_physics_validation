7032b63e64a7c3ee7fcec205bf1d31cf51dc8206
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 15:45:27 +0200

    Update init.sh

--- ci-scripts/init.sh
+++ ci-scripts/init.sh
@@ -1,5 +1,13 @@
 source config.cfg
-
+echo
+echo "CONFIGURATION:"
+echo "  KEY4HEP_SETUP   = $KEY4HEP_SETUP"
+echo "  NUMBER_OF_EVENTS= $NUMBER_OF_EVENTS"
+echo "  WORKAREA        = $WORKAREA"
+echo "  INPUTPROCESS    = $INPUTPROCESS"
+echo "  INFILENAME      = $INFILENAME"
+echo "  OUTFILEBASE     = $OUTFILEBASE"
+echo
 echo "SETUP PHASE:"
 # only source the setup if it is not set up yet
 [ -z "$KEY4HEP_STACK" ] && source "$KEY4HEP_SETUP"

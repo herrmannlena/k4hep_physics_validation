27aff4c91bc5410e14c89bd490f8000bc3742395
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 16:07:45 +0200

    Create reconstruction.sh

---
+++ ci-scripts/reconstruction.sh
@@ -0,0 +1,9 @@
+cd "${CLDCONFIG}/share/CLDConfig"
+echo "RECONSTRUCTION PHASE:"
+k4run CLDReconstruction.py \
+    --inputFiles "${WORKAREA}/${OUTFILEBASE}_SIM.root" \
+    --outputBasename "${WORKAREA}/${OUTFILEBASE}" 
+
+cd "${WORKAREA}"
+
+echo "TAGGING PHASE:""

324e9226ddc4cbde7b992cfaa22af66b8bf09f8e
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 15:24:21 +0200

    Update simulation.sh

--- ci-scripts/simulation.sh
+++ ci-scripts/simulation.sh
@@ -1,9 +1,10 @@
 # # simulation phase
 echo "SIMULATION PHASE:"
-
+INFILENAME="${WORKAREA}/../data/${INPUTPROCESS}_GEN.stdhep"
+OUTFILEBASE="TEST_${INPUTPROCESS}_$(date +%Y%m%d)"
 # cd "${CLDCONFIG}/share/CLDConfig"
 echo "Starting simulation..."
-
+cd "${CLDCONFIG}/share/CLDConfig"
 ddsim --steeringFile "${CLDCONFIG}/share/CLDConfig/cld_steer.py" \
       --compactFile "${K4GEO}/FCCee/CLD/compact/CLD_o2_v07/CLD_o2_v07.xml" \
       --numberOfEvents "${NUMBER_OF_EVENTS}" \

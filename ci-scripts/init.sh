ec4b5be6287853731766e0932540880126d586e3
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 14:47:24 +0200

    Update init.sh

--- ci-scripts/init.sh
+++ ci-scripts/init.sh
@@ -6,5 +6,8 @@ echo "Working area: $WORKAREA"
 echo "Input: $INFILENAME"
 echo "Output: $OUTFILENAME"
 
+set -e
 
+# setup phase
+echo "Setting up software stack"
 [ -z "$KEY4HEP_STACK" ] && source "$KEY4HEP_SETUP"

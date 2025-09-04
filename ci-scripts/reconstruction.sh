0533a767455416fa7c63c77d6a0435cf8a42b8ad
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 17:26:30 +0200

    Update reconstruction.sh

--- ci-scripts/reconstruction.sh
+++ ci-scripts/reconstruction.sh
@@ -2,7 +2,7 @@ cd "${CLDCONFIG}/share/CLDConfig"
 echo "RECONSTRUCTION PHASE:"
 k4run CLDReconstruction.py \
     --inputFiles "${WORKAREA}/${OUTFILEBASE}_SIM.root" \
-    --outputBasename "${WORKAREA}/${OUTFILEBASE}" 
+    --outputBasename "${WORKAREA}/${OUTFILEBASE}" \
     --num-events -1
 cd "${WORKAREA}"
 

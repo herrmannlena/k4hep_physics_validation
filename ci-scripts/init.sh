40615ad285e3d869ae8feb848b2410396a278d00
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 14:41:48 +0200

    Create init.sh

---
+++ ci-scripts/init.sh
@@ -0,0 +1,10 @@
+source ./config.cfg
+
+# Debugging info
+echo "Using Key4HEP setup: $KEY4HEP_SETUP"
+echo "Working area: $WORKAREA"
+echo "Input: $INFILENAME"
+echo "Output: $OUTFILENAME"
+
+
+[ -z "$KEY4HEP_STACK" ] && source "$KEY4HEP_SETUP"

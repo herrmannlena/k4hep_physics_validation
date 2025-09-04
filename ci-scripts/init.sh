cf8f2a97a617b48d7c4f6c7d2020995de3b3aea1
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 15:44:33 +0200

    Update init.sh

--- ci-scripts/init.sh
+++ ci-scripts/init.sh
@@ -1,8 +1,6 @@
-source ./config.cfg
+source config.cfg
 
-WORKAREA=$PWD
-INFILENAME="${WORKAREA}/../data/${INPUTPROCESS}_GEN.stdhep"
-OUTFILEBASE="TEST_${INPUTPROCESS}_$(date +%Y%m%d)"
+echo "SETUP PHASE:"
+# only source the setup if it is not set up yet
+[ -z "$KEY4HEP_STACK" ] && source "$KEY4HEP_SETUP"
 
-
-[ -z "$KEY4HEP_STACK" ] && source KEY4HEP_SETUP

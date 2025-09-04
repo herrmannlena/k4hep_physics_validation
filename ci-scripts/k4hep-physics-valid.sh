ebf93cd7d890c7c96c90462d3d599f6f7aa187a3
Birgit Stapf <birgit.sylvia.stapf@cern.ch>
2025-09-04 15:01:03 +0200

    add reco commands

--- ci-scripts/k4hep-physics-valid.sh
+++ ci-scripts/k4hep-physics-valid.sh
@@ -5,15 +5,15 @@
 #some fixed variables for now:
 NUMBER_OF_EVENTS=10
 WORKAREA=$PWD
-INFILEBASE="wzp6_ee_mumuH_ecm240_GEN"
-INFILENAME="${INFILEBASE}.stdhep"
-OUTFILENAME="tester_${INFILENAME}_$(date +%Y%m%d).root"
+INPUTPROCESS="wzp6_ee_mumuH_ecm240"
+INFILENAME="${WORKAREA}/../data/${INPUTPROCESS}_GEN.stdhep"
+OUTFILEBASE="TEST_${INPUTPROCESS}_$(date +%Y%m%d)"
 
 # enable exit on error to check correct script execution from within pipeline
 set -e
 
 # setup phase
-echo "Setting up software stack"
+echo "SETUP PHASE:"
 # only source the setup if it is not set up yet
 [ -z "$KEY4HEP_STACK" ] && source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
@@ -28,17 +28,26 @@ echo "SIMULATION PHASE:"
 
 # cd "${CLDCONFIG}/share/CLDConfig"
 echo "Starting simulation..."
-
-ddsim --steeringFile "${CLDCONFIG}/share/CLDConfig/cld_steer.py" \
-      --compactFile "${K4GEO}/FCCee/CLD/compact/CLD_o2_v07/CLD_o2_v07.xml" \
-      --numberOfEvents "${NUMBER_OF_EVENTS}" \
-      --inputFiles ../data/${INFILENAME} \
-      --outputFile "${WORKAREA}/${OUTFILENAME}" 
+cd "${CLDCONFIG}/share/CLDConfig"
+# ddsim --steeringFile cld_steer.py \
+#       --compactFile "${K4GEO}/FCCee/CLD/compact/CLD_o2_v07/CLD_o2_v07.xml" \
+#       --numberOfEvents "${NUMBER_OF_EVENTS}" \
+#       --inputFiles "${INFILENAME}" \
+#       --outputFile "${WORKAREA}/${OUTFILEBASE}_SIM.root" 
 
 echo "Simulation execution finished"
 
+# # simulation phase
+echo "RECONSTRUCTION PHASE:"
+# echo "${CLDCONFIG}/share/CLDConfig/CLDReconstruction.py"
+k4run CLDReconstruction.py \
+    --inputFiles "${WORKAREA}/${OUTFILEBASE}_SIM.root" \
+    --outputBasename "${WORKAREA}/${OUTFILEBASE}" 
 
-# cd "${WORKAREA}/${GEOMETRY}/${VERSION}"
+cd "${WORKAREA}"
+
+echo "TAGGING PHASE:"
+
 
 
 # # analyze simulation file

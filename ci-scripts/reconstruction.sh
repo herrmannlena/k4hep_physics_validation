600e265cde5861360d6660f71d3e2e1dce6e6b05
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 18:18:21 +0200

    Update reconstruction.sh

--- ci-scripts/reconstruction.sh
+++ ci-scripts/reconstruction.sh
@@ -3,7 +3,8 @@ echo "RECONSTRUCTION PHASE:"
 k4run CLDReconstruction.py \
     --inputFiles "${WORKAREA}/${OUTFILEBASE}_SIM.root" \
     --outputBasename "${WORKAREA}/${OUTFILEBASE}" \
-    --num-events -1
+    --num-events -1 \
+    --enableLCFIJet 
 cd "${WORKAREA}"
 
 echo "Reconstruction was successful!"

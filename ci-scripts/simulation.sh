28c24b2dfea03eed7f47ab284062b50fd67696e6
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 15:46:15 +0200

    Update simulation.sh

--- ci-scripts/simulation.sh
+++ ci-scripts/simulation.sh
@@ -4,10 +4,10 @@ echo "SIMULATION PHASE:"
 # cd "${CLDCONFIG}/share/CLDConfig"
 echo "Starting simulation..."
 cd "${CLDCONFIG}/share/CLDConfig"
-ddsim --steeringFile "${CLDCONFIG}/share/CLDConfig/cld_steer.py" \
-      --compactFile "${K4GEO}/FCCee/CLD/compact/CLD_o2_v07/CLD_o2_v07.xml" \
-      --numberOfEvents "${NUMBER_OF_EVENTS}" \
-      --inputFiles ../data/${INFILENAME} \
-      --outputFile "${WORKAREA}/${OUTFILENAME}" 
+ddsim --steeringFile cld_steer.py \
+       --compactFile "${K4GEO}/FCCee/CLD/compact/CLD_o2_v07/CLD_o2_v07.xml" \
+       --numberOfEvents "${NUMBER_OF_EVENTS}" \
+       --inputFiles "${INFILENAME}" \
+       --outputFile "${WORKAREA}/${OUTFILEBASE}_SIM.root" 
 
 echo "Simulation execution finished"

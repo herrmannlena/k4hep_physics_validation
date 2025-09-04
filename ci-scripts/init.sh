0ce39eb67e18f23f4721b83b887ab8e61603cf0e
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-04 15:33:06 +0200

    Update init.sh

--- ci-scripts/init.sh
+++ ci-scripts/init.sh
@@ -3,14 +3,6 @@ source ./config.cfg
 WORKAREA=$PWD
 INFILENAME="${WORKAREA}/../data/${INPUTPROCESS}_GEN.stdhep"
 OUTFILEBASE="TEST_${INPUTPROCESS}_$(date +%Y%m%d)"
-# Debugging info
-echo "Using Key4HEP setup: $KEY4HEP_SETUP"
-echo "Working area: $WORKAREA"
-echo "Input: $INFILENAME"
-echo "Output: $OUTFILENAME"
 
-#set -e
 
-# setup phase
-echo "Setting up software stack"
-[ -z "$KEY4HEP_STACK" ] && source "$KEY4HEP_SETUP"
+[ -z "$KEY4HEP_STACK" ] && source KEY4HEP_SETUP

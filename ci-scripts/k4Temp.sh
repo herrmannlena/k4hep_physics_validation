ce0354b8eefbf9383b77cf3639c8f5f8be792c25
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2025-09-05 14:17:21 +0200

    Create k4Temp.sh

---
+++ ci-scripts/k4Temp.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/key4hep/k4-project-template
+cd k4-project-template
+#source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
+k4_local_repo
+mkdir build install
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install
+make -j 4 install
